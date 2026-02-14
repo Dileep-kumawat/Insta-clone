8a10b146950e9ff98fa3190b0301b9be7783998b
Dileep <dileepkumawat525@gmail.com>
2026-02-14 14:07:59 +0530

    Implement post creation with JWT authentication and image upload handling

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -1,4 +1,6 @@
-const ImageKit = require("@imagekit/nodejs")
+const ImageKit = require("@imagekit/nodejs");
+const postModel = require("../models/post.model");
+const jwt = require("jsonwebtoken");
 const { toFile } = require("@imagekit/nodejs")
 
 const imagekit = new ImageKit({
@@ -7,13 +9,29 @@ const imagekit = new ImageKit({
 
 const postCreateController = async (req, res) => {
 
+    let decoded;
+    try {
+        decoded = jwt.verify(req.cookies.jwt_token, process.env.JWT_SECRET);
+    } catch (err) {
+        return res.status(401).json({
+            "msg": "The token is unauthorized"
+        });
+    }
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
         fileName: "Test"
     });
 
-    res.json({
-        file
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    });
+
+    res.status(201).json({
+        "msg": "The post is created successfully",
+        post
     });
 }
 
