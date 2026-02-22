d9bafb3870ec92add9aad029c80618a7913bf04b
Dileep <dileepkumawat525@gmail.com>
2026-02-22 06:31:41 +0530

    Login and Register pages initialized

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const authRoute = require("./routes/auth.route");
 const postRouter = require("./routes/post.route");
 const followRouter = require("./routes/follow.route");
@@ -9,6 +10,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    credentials: true,
+    origin : process.env.FRONTEND_ENDPOINT
+}));
 
 /**
  * Auth routes
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,9 +1,70 @@
+import { useState } from "react"
+import axios from 'axios'
 
 const Register = () => {
+    const [form, setForm] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    async function formSubmitHandler(e) {
+        e.preventDefault();
+
+        const res = await axios.post(import.meta.env.VITE_BACKEND_ENDPOINT + '/api/auth/register', {
+            username: form.username,
+            email: form.email,
+            password: form.password
+        }, { withCredentials: true });
+
+        console.log(res);
+    }
+
     return (
-        <div>
-            Register page
-        </div>
+        <main>
+            <div className="container">
+                <h2>Register : </h2>
+                <form onSubmit={formSubmitHandler}>
+                    <input
+                        onChange={(e) => {
+                            setForm(prev => {
+                                return {
+                                    ...prev,
+                                    username: e.target.value
+                                }
+                            })
+                        }}
+                        value={form.username}
+                        type="text"
+                        placeholder="Enter your username" />
+                    <input
+                        onChange={(e) => {
+                            setForm(prev => {
+                                return {
+                                    ...prev,
+                                    email: e.target.value
+                                }
+                            })
+                        }}
+                        value={form.email}
+                        type="email"
+                        placeholder="Enter your email" />
+                    <input
+                        onChange={(e) => {
+                            setForm(prev => {
+                                return {
+                                    ...prev,
+                                    password: e.target.value
+                                }
+                            })
+                        }}
+                        value={form.password}
+                        type="password"
+                        placeholder="Enter the password" />
+                    <input type="submit" />
+                </form>
+            </div>
+        </main>
     )
 }
 
